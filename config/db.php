ba94cf50030e9611af23d5af8c6b66f5791e5e14
aryapatilbackup <aryapatil3455@gmail.com>
2026-04-19 21:13:15 +0530

    Fix Railway SSL DB connection

--- config/db.php
+++ config/db.php
@@ -5,7 +5,19 @@ $password = "YJKJKjXhOdKHyLLGcFhfcJ1InnosvSv";
 $database = "railway";
 $port = 19660;
 
-$conn = mysqli_connect($host, $user, $password, $database, $port);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+mysqli_real_connect(
+    $conn,
+    $host,
+    $user,
+    $password,
+    $database,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
