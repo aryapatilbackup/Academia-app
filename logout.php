5c1a33ae4fe22657edc4ab3eab15d0ab4088adb4
aryapatilbackup <aryapatil3455@gmail.com>
2026-04-27 00:33:19 +0530

    Fix logout by clearing remember-me cookie along with session

--- logout.php
+++ logout.php
@@ -1,7 +1,17 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// remove session
 session_unset();
 session_destroy();
 
+// ❌ remove remember-me cookie
+if (isset($_COOKIE['user_id'])) {
+    setcookie("user_id", "", time() - 3600, "/");
+}
+
 header("Location: login.php");
 exit;
+?>
